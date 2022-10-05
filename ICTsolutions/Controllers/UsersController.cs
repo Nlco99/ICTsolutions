12ae0c4e5073e49b507642b6621dc8333ce6b658
nico <94600701+Nlco99@users.noreply.github.com>
2022-10-05 22:30:26 +0200

    Adding Users controller and Index view.

---
+++ ICTsolutions/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ICTsolutions.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+
+    }
+}
