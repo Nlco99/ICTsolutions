40fae34e376aaa49c297160d71b15a3dc914471f
nico <94600701+Nlco99@users.noreply.github.com>
2022-10-05 22:57:40 +0200

    (Out of scope Lecture) UnitOfWork Repository.

---
+++ ICTsolutions/Core/IRepositories/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿//out of scope tutorial
+
+using ICTsolutions.Core.Repositories;
+
+namespace ICTsolutions.Core.IRepositories
+{
+    public interface IUnitOfWork
+    {
+        IUserRepository User { get; }
+
+    }
+}
---
+++ ICTsolutions/Repositories/UnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using ICTsolutions.Core.IRepositories;
+using ICTsolutions.Core.Repositories;
+
+namespace ICTsolutions.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+
+        public IUserRepository User { get; }
+
+
+        //implement constructor - inject IUserRepository and initialise variable (user)
+        public UnitOfWork(IUserRepository user)
+        {
+            User = user;
+        }
+    }
+}
